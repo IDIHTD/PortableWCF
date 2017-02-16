d4f0946287a905372dfbd250f1490104da5c2ecb
mahui <dangnicaikuqi@163.com>
2017-02-16 15:08:51 +0800

    更新XMLBUG修改

--- AutomaticUpdatesWCF/AutomaticUpdateImplement.svc.cs
+++ AutomaticUpdatesWCF/AutomaticUpdateImplement.svc.cs
@@ -42,6 +42,7 @@ namespace AutomaticUpdatesWCF
                     var appEntity= FileProcessingHelper.GetTFromXML<ApplicationEntity>(getTxtString);
                     if(appEntity!=null)
                     {
+                        AppList = new List<ApplicationEntity>();
                         AppList.Add(appEntity);
                         return appEntity.AppVersion;
                     }
